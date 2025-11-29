2aa35d1b5eda7cdc931e247356f857e32e629a6b
ytbob <ytbob@yandex.ru>
2025-11-29 11:25:34 +0500

    fix clients 2

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,7 +1,7 @@
 import axios from 'axios'
 import { useAuthStore } from '@/store/authStore'
 
-const API_URL = import.meta.env.VITE_API_URL || 'https://musicme.ru/'
+const API_URL = import.meta.env.VITE_API_URL || 'https://musicme.ru'
 
 const apiClient = axios.create({
   baseURL: `${API_URL}/api/v1`,
